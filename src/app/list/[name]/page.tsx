0e10ec4f1625df3eba24e9948cdaee490d5fe112
Minseok Kim^C <ksj5147@gmail.com>
2025-04-12 00:34:34 -0700

    11111

--- src/app/list/[name]/page.tsx
+++ src/app/list/[name]/page.tsx
@@ -1,12 +1,9 @@
 // app/list/[name]/page.tsx
-'use client' // 클라이언트 컴포넌트로 선언
-
-import { use } from 'react'
 import BuyLinks from './BuyLinks'
 
 // PageProps 타입 정의
 type PageProps = {
-  params: Promise<{ name: string }>
+  params: Promise<{ name: string }> // params가 Promise임을 반영
   searchParams?: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
@@ -23,8 +20,7 @@ type ListData = {
   books: Book[]
 }
 
-// 데이터 페칭 함수
-async function fetchListData (name: string): Promise<ListData> {
+async function getListData (name: string): Promise<ListData> {
   const response = await fetch(
     `https://books-api.nomadcoders.workers.dev/list?name=${name}`,
     { cache: 'no-store' }
@@ -34,44 +30,50 @@ async function fetchListData (name: string): Promise<ListData> {
   return json.results
 }
 
-export default function ListPage ({ params }: PageProps) {
-  // use 훅으로 params 풀기
-  const resolvedParams = use(params)
+// ListPage는 async로 선언
+export default async function ListPage ({ params }: PageProps) {
+  // params를 await로 풀기
+  const resolvedParams = await params
 
   // resolvedParams.name이 string임을 보장
   if (!resolvedParams || !resolvedParams.name) {
     return <div className='page'>에러: 리스트 이름을 찾을 수 없어요!</div>
   }
 
-  // 클라이언트에서 데이터 페칭
-  const listDataPromise = fetchListData(resolvedParams.name)
-  const listData = use(listDataPromise)
+  try {
+    const listData = await getListData(resolvedParams.name)
 
-  return (
-    <div className='page'>
-      <h1 className='title'>{listData.list_name}</h1>
-      <div className='list-container'>
-        {listData.books.map(book => (
-          <div key={book.rank} className='book-item'>
-            <img
-              src={book.book_image}
-              alt={book.title}
-              className='book-image'
-            />
-            <div className='book-info'>
-              <h3>{book.title}</h3>
-              <p>
-                Author: <br />
-                {book.author}
-              </p>
-              <BuyLinks
-                amazonUrl={book.amazon_product_url}
-                title={book.title}
+    return (
+      <div className='page'>
+        <h1 className='title'>{listData.list_name}</h1>
+        <div className='list-container'>
+          {listData.books.map(book => (
+            <div key={book.rank} className='book-item'>
+              <img
+                src={book.book_image}
+                alt={book.title}
+                className='book-image'
               />
+              <div className='book-info'>
+                <h3>{book.title}</h3>
+                <p>
+                  Author: <br />
+                  {book.author}
+                </p>
+                <BuyLinks
+                  amazonUrl={book.amazon_product_url}
+                  title={book.title}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return <div className='page'>에러: {error.message}</div>
+    }
+    return <div className='page'>알 수 없는 에러가 발생했어요!</div>
+  }
 }
